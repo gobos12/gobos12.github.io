6670e2709a151f3c5657b817aa47411efc69b4e8
Orion Dobos <oriondobos@gmail.com>
2024-06-28 17:55:23 -0700

    removing unnecessary imports

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Fade} from "react-reveal";
 import {greeting} from "../../details";
-import Button from "../../components/button/Button.scss";
 import "./Greeting.scss";
 
 export default function Greeting()
